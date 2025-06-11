367f88fe23aba79ffec5fa0cb87279ab983b2056
MasonG <shipperturtle@gmail.com>
2025-06-10 18:13:25 -0700

    adding junit jar file

--- SolarCalcTest.java
+++ SolarCalcTest.java
@@ -7,9 +7,28 @@ import java.lang.*;
  * JUnit Tests for SolarCalc
  */
 public class SolarCalcTest {
-	
-	public static void main (String[] args) {
+	private double radThree, radFour, radFive;
+	@BeforeEach 
+	void reset(){
+		double radThree = 3.0;
+		double radFour =  4.0;
+		double radFive = 5.0;
+	}
+	/**
+	 * 
+	 */
+	@Test
+	public void testCalculateInstallCostsMono(){
 		
 	}
+	
+	/**
+	 * 
+	 */
+	@Test
+	public void testCalculateInstallCostsPoly(){
+		
+	}
+	
 }
 
